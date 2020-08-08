0a7f721e0ad1b4bf3e0576b96990895f5c53c242
Jonathan Cavalcanti <jonathan.cavalcanti11@gmail.com>
2020-08-08 15:12:04 -0300

    Adicionando e estilizando o Carrousel

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import Home from './pages/cadastro/home';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import CadastroVideo from '../src/pages/cadastro/video/index';
+import CadastroCategoria from './pages/cadastro/Categoria';
             //Componentes da Lib
 //Desafio master descrição;
 const Pagina404 = () => (<div>Página 404</div>)
@@ -12,7 +13,8 @@ ReactDOM.render(
   <BrowserRouter> 
     <Switch>
       <Route path="/" component={Home} exact />
-      <Route path="/cadastro/video" component={CadastroVideo} />      
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />      
       <Route component={ Pagina404 } /> 
     </Switch>     
   </BrowserRouter>,
